d7de2b131f5e2462092c6d60fb9aab9e7fbca272
Keith Gaines <patrick.keith.gaines@gmail.com>
2022-11-22 09:29:55 -0600

    added text watermark

--- main.py
+++ main.py
@@ -14,3 +14,12 @@ upload = filedialog.askopenfilename(initialdir='/',
 
 img = ImageTk.PhotoImage(Image.open(upload))
 
+h = img.height()
+w = img.width()
+canvas = tk.Canvas(window, width=w, height=h, highlightthickness=0)
+canvas.create_image(w/2, h/2, anchor="center", image=img)
+canvas.pack()
+
+canvas.create_text(w/2, h/2, text="This is a text watermark", fill="black", font=("Times New Roman", 30))
+canvas.pack()
+tk.mainloop()
