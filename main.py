d022eef4f59e4c62e502e37a26e0cbd5eec3cfb2
Keith Gaines <patrick.keith.gaines@gmail.com>
2022-11-22 09:41:29 -0600

    added save as png

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import tkinter as tk
 from tkinter import filedialog
-from PIL import Image, ImageTk, ImageDraw, ImageFont
+from PIL import Image, ImageTk, ImageDraw, ImageFont, ImageGrab
+
+
+# def save_as_png(canvas,fileName):
+#     # save postscipt image
+#     canvas.postscript(file = fileName + '.eps')
+#     # use PIL to convert to PNG
+#     img = Image.open(fileName + '.eps')
+#     img.save(fileName + '.png', 'png')
 
 window = tk.Tk()
 window.title("Image Watermarker")
@@ -22,4 +30,6 @@ canvas.pack()
 
 canvas.create_text(w/2, h/2, text="This is a text watermark", fill="black", font=("Times New Roman", 30))
 canvas.pack()
+
+# save_as_png()
 tk.mainloop()
