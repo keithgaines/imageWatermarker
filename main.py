7ba4fbd4b75cb72fd9b155d429b2ccb60c978f0a
keithgaines <93451842+keithgaines@users.noreply.github.com>
2022-12-20 10:42:52 -0600

    created canvas with image dimensions

--- main.py
+++ main.py
@@ -2,34 +2,55 @@ import tkinter as tk
 from tkinter import filedialog
 from PIL import Image, ImageTk, ImageDraw, ImageFont, ImageGrab
 
+ 
 
-# def save_as_png(canvas,fileName):
-#     # save postscipt image
-#     canvas.postscript(file = fileName + '.eps')
-#     # use PIL to convert to PNG
-#     img = Image.open(fileName + '.eps')
-#     img.save(fileName + '.png', 'png')
+def save_as_png(canvas, file_name):
+    # Save postscript image
+    canvas.postscript(file=file_name + '.eps')
+    # Use PIL to convert to PNG
+    img = Image.open(file_name + '.eps')
+    img.save(file_name + '.png', 'png')
 
+ 
+
+# Create Tkinter window and canvas
 window = tk.Tk()
 window.title("Image Watermarker")
 canvas = tk.Canvas(window)
-canvas.pack()
-canvas.place()
 
+ 
+
+# Prompt user to select an image file
 upload = filedialog.askopenfilename(initialdir='/',
                                     title="Select an Image",
                                     filetypes=(('png files', '*.png'), ('jpeg files', '*.jpeg')))
 
-img = ImageTk.PhotoImage(Image.open(upload))
+ 
 
+# Load image and get dimensions
+img = ImageTk.PhotoImage(Image.open(upload))
 h = img.height()
 w = img.width()
+
+ 
+
+# Create canvas with image dimensions
 canvas = tk.Canvas(window, width=w, height=h, highlightthickness=0)
 canvas.create_image(w/2, h/2, anchor="center", image=img)
 canvas.pack()
 
+ 
+
+# Add watermark text to canvas
 canvas.create_text(w/2, h/2, text="This is a text watermark", fill="black", font=("Times New Roman", 30))
 canvas.pack()
 
-# save_as_png()
+ 
+
+# Save canvas as a PNG file
+save_as_png(canvas, "watermarked_image")
+
+ 
+
+# Start Tkinter event loop
 tk.mainloop()
